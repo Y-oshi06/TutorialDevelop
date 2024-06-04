f60fbce16ee7d7adfdb293b6f209515a5bc81485
Y-oshi06 <black.gorilla0930@gmail.com>
2024-06-04 14:57:54 +0900

    chapter5完了

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -15,29 +15,23 @@ import lombok.Data;
 @Table(name = "user")
 public class User {
 
-    /** 性別用の列挙型 */
     public static enum Gender {
         男性, 女性
     }
 
-    /** 主キー。自動生成 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    /** 名前。20桁。null不許可 */
     @Column(length = 20, nullable = false)
     private String name;
 
-    /** 性別。2桁。列挙型（文字列） */
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    /** 年齢 */
     private Integer age;
 
-    /** メールアドレス。50桁。null許可 */
     @Column(length = 50)
     private String email;
 
