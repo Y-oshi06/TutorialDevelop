a7d40829b62f412414446b0d26de2ea52b250b40
Y-oshi06 <black.gorilla0930@gmail.com>
2024-06-12 05:47:14 +0900

    chapter12完了

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,9 +21,22 @@ public class UserService {
 
         return userRepository.findAll();
     }
+    
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
 
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
+    
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id: idck) {
+            userRepository.deleteById(id);
+        }
+    }
+    
+    
 }
